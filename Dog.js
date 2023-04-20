28603798f5c37acdf36c2ae6e7ef3db80e1a5e2a
Youkwhan <Youkwhan@gmail.com>
2023-04-20 15:06:16 -0400

    js

--- Dog.js
+++ Dog.js
@@ -1,1 +1,29 @@
 // Create the Dog class here
+
+export default class Dog {
+	constructor(data) {
+		Object.assign(this, data);
+	}
+
+	getBadgeHTML() {
+		return this.hasBeenLiked
+			? `<img class="like-dislike" src="./images/badge-like.png" alt="badge"></img>`
+			: this.hasBeenSwiped
+			? `<img class="like-dislike" src="./images/badge-nope.png" alt="badge"></img>`
+			: "";
+	}
+
+	cardHTML() {
+		const { name, avatar, age, bio } = this;
+		const badge = this.getBadgeHTML();
+		return `
+      <img class="hero" src="${avatar}">
+		${badge}
+		<div class="hero-content">
+			<h1 class="hero-title">${name}, ${age}</h1>
+			<p class="hero-body">${bio}</p>
+					
+		</div>
+      `;
+	}
+}
--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Remember to import the data and Dog class!
+// Remember to import the data and Dog class!'
+import Dog from "./Dog.js";
+import { dogs } from "./data.js";
+
+/*What do we need to do?
+- when button clicked
+   - display badge
+   - delay change dog
+*/
+function noMatch() {
+	document.getElementById("card").innerHTML = `
+   <div><h2>No more matches</h2></div>`;
+}
+
+function newMatch(badge) {
+	setTimeout(() => {
+		badge = !badge;
+		if (dogs) {
+			hero = new Dog(dogs.shift());
+			render();
+		} else {
+			noMatch();
+		}
+	}, 500);
+}
+
+document.querySelector(".icon-heart").addEventListener("click", () => {
+	hero.hasBeenLiked = !hero.hasBeenLiked;
+	render();
+	// document.querySelector(".like-dislike").style.opacity = "1";
+	newMatch(hero.hasBeenLiked);
+});
+document.querySelector(".icon-cross").addEventListener("click", () => {
+	// document.querySelector(".like-dislike").style.opacity = "1";
+	hero.hasBeenSwiped = !hero.hasBeenSwiped;
+	render();
+	newMatch(hero.hasBeenSwiped);
+});
+
+function render() {
+	document.getElementById("card").innerHTML = hero.cardHTML();
+}
+
+let hero = newMatch();
+render();
